8879a9c11f8f5e34e8d0071b69cbb3464ebdb781
Deepak <ddeepakkumar775@gmail.com>
2020-06-09 23:27:26 +0530

    code formated

--- src/main/java/com/medplus/view/AccountStatementPdfView.java
+++ src/main/java/com/medplus/view/AccountStatementPdfView.java
@@ -14,15 +14,15 @@ import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import com.medplus.dto.AccountStatementDto;
 
-public class AccountStatementPdfView extends AbstractPdfView{
+public class AccountStatementPdfView extends AbstractPdfView {
 
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		List<AccountStatementDto> statements=null;
-		PdfPTable table=new PdfPTable(6);
+		List<AccountStatementDto> statements = null;
+		PdfPTable table = new PdfPTable(6);
 		table.addCell("txNo");
 		table.addCell("transactionDate");
 		table.addCell("remarks");
@@ -30,18 +30,18 @@ public class AccountStatementPdfView extends AbstractPdfView{
 		table.addCell("credit");
 		table.addCell("availableBalance");
 		document.add(table);
-		statements=(List<AccountStatementDto>) model.get("statements");
-		for(int i=0;i<statements.size();i++) {
-			AccountStatementDto dto=statements.get(i);
-			SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+		statements = (List<AccountStatementDto>) model.get("statements");
+		for (int i = 0; i < statements.size(); i++) {
+			AccountStatementDto dto = statements.get(i);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			table.addCell(String.valueOf(dto.getTxNo()));
 			table.addCell(sdf.format(dto.getTransactionDate()));
 			table.addCell(dto.getRemarks());
 			table.addCell(String.valueOf(dto.getDebit()));
 			table.addCell(String.valueOf(dto.getCredit()));
 			table.addCell(String.valueOf(dto.getAvailableBalance()));
-			document.add(table);			
-		}			
+			document.add(table);
+		}
 	}
 
 }
