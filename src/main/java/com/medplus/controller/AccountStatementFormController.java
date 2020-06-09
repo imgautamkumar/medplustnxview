dd3977eb39edb1555a97171867b2c4e3591d20b2
Deepak <ddeepakkumar775@gmail.com>
2020-06-09 23:35:05 +0530

    code formated

--- src/main/java/com/medplus/controller/AccountStatementFormController.java
+++ src/main/java/com/medplus/controller/AccountStatementFormController.java
@@ -24,11 +24,11 @@ public class AccountStatementFormController extends SimpleFormController {
 
 		List<AccountStatementDto> statements = null;
 		ModelAndView mav = null;
-		AccountStatementCommand aCommand=null;
-		
+		AccountStatementCommand aCommand = null;
+
 		statements = new ArrayList<>();
-		mav=new ModelAndView();
-		aCommand=(AccountStatementCommand) command;
+		mav = new ModelAndView();
+		aCommand = (AccountStatementCommand) command;
 		statements.add(new AccountStatementDto(1584, new Date(), "db mall", 500f, 0, 1505));
 		statements.add(new AccountStatementDto(1454, new Date(), "pentaloons", 1005f, 0, 900));
 		statements.add(new AccountStatementDto(1544, new Date(), "hp oil petrol", 200f, 0, 700));
@@ -36,20 +36,20 @@ public class AccountStatementFormController extends SimpleFormController {
 		statements.add(new AccountStatementDto(1654, new Date(), "zomato", 250f, 0, 400));
 		mav.addObject(aCommand);
 		mav.addObject("statements", statements);
-		if(aCommand.getStatementType().equals("normal")) {
+		if (aCommand.getStatementType().equals("normal")) {
 			mav.setViewName("account-statement");
 			return mav;
 		}
-		if(aCommand.getStatementType().equals("pdf")) {
+		if (aCommand.getStatementType().equals("pdf")) {
 			mav.setViewName("account-statement-pdf");
 			return mav;
 		}
-		
-		if(aCommand.getStatementType().equals("xls")) {
+
+		if (aCommand.getStatementType().equals("xls")) {
 			mav.setViewName("account-statement-xls");
 			return mav;
 		}
-		
+
 		return mav;
 	}
 
